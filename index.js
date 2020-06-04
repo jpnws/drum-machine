2166e3736d6768e1b6f927712f2312e49cda2e5d
Ji Park <61026408+jiparkdev@users.noreply.github.com>
2020-06-03 22:05:57 -0700

    Add drum pad component. Map buttons to mp3 files.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+class DrumPad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.playPad = this.playPad.bind(this);
+  }
+
+  playPad() {
+    document.getElementById(this.props.padName).play();
+  }
+
+  render() {
+    let audioPath = "audio/" + this.props.padName + ".mp3";
+    console.log(audioPath);
+    return (
+      <div>
+        <button onClick={this.playPad}>{this.props.padName}</button>
+        <audio id={this.props.padName} src={audioPath}></audio>
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -6,7 +28,15 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Drum Machine</h1>
+        <DrumPad padName="Q" />
+        <DrumPad padName="W" />
+        <DrumPad padName="E" />
+        <DrumPad padName="A" />
+        <DrumPad padName="S" />
+        <DrumPad padName="D" />
+        <DrumPad padName="Z" />
+        <DrumPad padName="X" />
+        <DrumPad padName="C" />
       </div>
     );
   }
