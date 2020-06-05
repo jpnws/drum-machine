7cd8a51273412abddc5dced8ca2679da09f5516b
Ji Park <61026408+jiparkdev@users.noreply.github.com>
2020-06-04 22:45:36 -0700

    Finish up styling and meet all ffc requirements.

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ class DrumPad extends React.Component {
   }
 
   playPad() {
-    console.log(this.props.padName);
     document.getElementById(this.props.padName).play();
   }
 
   render() {
-    let audioPath = "audio/" + this.props.padName + ".mp3";
+    // let audioPath = "audio/" + this.props.padName + ".mp3";
+    let audioPath =
+      "https://raw.githubusercontent.com/jiparkdev/drum-machine/master/audio/" +
+      this.props.padName +
+      ".mp3";
     let uniqueID = "pad" + this.props.padName;
     return (
-      <div className="drum-pad" id={uniqueID} onClick={this.playPad}>
+      <div
+        className="drum-pad"
+        id={uniqueID}
+        onClick={(event) => {
+          this.playPad();
+          this.props.padClick(event);
+        }}
+      >
         {this.props.padName}
         <audio className="clip" id={this.props.padName} src={audioPath}></audio>
       </div>
@@ -24,22 +34,41 @@ class DrumPad extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      displayText: "",
+    };
+    document.addEventListener("keydown", (event) => {
+      document.getElementById(event.key.toUpperCase()).play();
+      this.setState({
+        displayText: event.key,
+      });
+    });
+    this.handlePadClick = this.handlePadClick.bind(this);
+  }
+
+  handlePadClick(event) {
+    this.setState({
+      displayText: event.target.id,
+    });
   }
 
   render() {
     return (
       <div className="drum-platform">
         <div className="drum-machine-title">Drum Machine</div>
-        <div className="drum-pad-group" id="display">
-          <DrumPad padName="Q" />
-          <DrumPad padName="W" />
-          <DrumPad padName="E" />
-          <DrumPad padName="A" />
-          <DrumPad padName="S" />
-          <DrumPad padName="D" />
-          <DrumPad padName="Z" />
-          <DrumPad padName="X" />
-          <DrumPad padName="C" />
+        <div className="drum-pad-group">
+          <DrumPad padName="Q" padClick={this.handlePadClick} />
+          <DrumPad padName="W" padClick={this.handlePadClick} />
+          <DrumPad padName="E" padClick={this.handlePadClick} />
+          <DrumPad padName="A" padClick={this.handlePadClick} />
+          <DrumPad padName="S" padClick={this.handlePadClick} />
+          <DrumPad padName="D" padClick={this.handlePadClick} />
+          <DrumPad padName="Z" padClick={this.handlePadClick} />
+          <DrumPad padName="X" padClick={this.handlePadClick} />
+          <DrumPad padName="C" padClick={this.handlePadClick} />
+        </div>
+        <div className="drum-display" id="display">
+          {this.state.displayText}
         </div>
       </div>
     );
